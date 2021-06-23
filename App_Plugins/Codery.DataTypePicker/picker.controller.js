12edfb482d56fa15ca37a3314abb6762dfe19d25
Tom van Enckevort <tom.v.enckevort@gmail.com>
2021-06-23 08:21:40 +0100

    Store data type key instead of ID.

--- App_Plugins/Codery.DataTypePicker/picker.controller.js
+++ App_Plugins/Codery.DataTypePicker/picker.controller.js
@@ -1,16 +1,25 @@
 angular.module('umbraco').controller('Codery.DataTypePickerController', function ($scope, editorState, dataTypeResource) {
 
-    if ($scope.model.value !== undefined && $scope.model.value !== null) {
-        $scope.model.value = parseInt($scope.model.value);
-    }
-
     $scope.dataTypes = [];
 
     // load all data types, apart from current to avoid inception
 
     dataTypeResource.getAll()
-        .then(function(data) {
+        .then(function (data) {
             $scope.dataTypes = _.filter(data, function (dt) { return dt.id !== editorState.current.id; });
+
+            if ($scope.model.value !== undefined && $scope.model.value !== null) {
+                var intValue = parseInt($scope.model.value);
+
+                if (!isNaN(intValue)) {
+                    // old int value, get the data type's GUID key instead
+                    var dataType = _.find(data, function (dt) { return dt.id === intValue; });
+
+                    if (dataType) {
+                        $scope.model.value = dataType.key;
+                    }
+                }
+            }
         });
 
 });
